de0d3c77f8a9ba5e15b4d730773e6ffe979603dc
Alen Mujkanović <mujkalen@gmail.com>
2025-01-20 19:57:54 +0100

    1.zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import database.DatabaseService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DatabaseService.createConnection();
+        deleteInvoiceAndItem(connection);
+    }
 
+    static void deleteInvoiceAndItem(Connection connection) throws SQLException {
 
+        try {
+            connection.setAutoCommit(false);
+            Statement stmt = connection.createStatement();
+
+            String deleteItemsQuery = "DELETE FROM Stavka WHERE RacunID=43659";
+            stmt.executeUpdate(deleteItemsQuery);
+            String deleteInvoiceQuery = "DELETE FROM Racun WHERE IDRacun=43659";
+            stmt.executeUpdate(deleteInvoiceQuery);
+            connection.commit();
+
+        } catch (SQLException e) {
+            connection.rollback();
+        }
     }
 }
