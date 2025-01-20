44d41d9ea64e3d7fc482b11341f56c2851abd3a8
Alen Mujkanović <mujkalen@gmail.com>
2025-01-20 20:58:56 +0100

    2.zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import database.DatabaseService;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection = DatabaseService.createConnection();
-        deleteInvoiceAndItem(connection);
+        //deleteInvoiceAndItem(connection);
+        createInvoice(connection);
     }
 
     static void deleteInvoiceAndItem(Connection connection) throws SQLException {
@@ -24,6 +23,47 @@ public class Main {
 
         } catch (SQLException e) {
             connection.rollback();
+        }
+    }
+
+    static void createInvoice(Connection connection) throws SQLException {
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement stmt;
+
+            String createInvoiceQuery = "INSERT INTO Racun(Datumizdavanja,BrojRacuna,KupacID)VALUES('01-20-2025','SO12345',216)";
+            stmt = connection.prepareStatement(createInvoiceQuery);
+            System.out.println("Invoice added");
+
+            String selectInvoiceIdQuery="SELECT RacunID FROM Racun WHERE BrojRacuna=SO12345";
+            ResultSet rs = stmt.executeQuery(selectInvoiceIdQuery);
+
+            String insertItemQuery ="INSERT INTO Stavka(RacunID,Kolicina,ProizvodID,CijenaPoKomadu,PopustUPostocima,UkupnaCijena)" +
+                    "VALUES(?,?,?,?,?,?)";
+            stmt = connection.prepareStatement(insertItemQuery);
+            stmt.setInt(1,rs.getInt("IDRacun"));
+            stmt.setInt(2,1);
+            stmt.setInt(3, 762);
+            stmt.setDouble(4, 419.4589);
+            stmt.setDouble(5, 0.00);
+            stmt.setDouble(6, 419.458900);
+            stmt.executeUpdate();
+            System.out.println("Item added");
+
+            String getQuantityQuery = "SELECT MinimalnaKolicinaNaSkladistu FROM Proizvod WHERE IDProizvod = ?";
+            stmt = connection.prepareStatement(getQuantityQuery);
+            stmt.setInt(1, 762);
+            rs = stmt.executeQuery();
+
+            String updateItemQuantity = "UPDATE Proizvod" +
+                    "MinimalnaKolicinaNaSkladistu=? WHERE IDProizvod=762";
+            stmt = connection.prepareStatement(updateItemQuantity);
+            stmt.setInt(1, rs.getInt("MinimalnaKolicinaNaSkladistu"));
+
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
         }
     }
 }
